194633020e414b176ae95ff9021efacdc04c61b3
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 11:28:35 -0600

    added the function to create a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -25,4 +25,15 @@ module.exports = {
       res.status(500).json(err);
     }
   },
-}
+  // Create a user
+  async createUser(req, res) {
+    try {
+      const user = await User.create(req.body);
+      res.json(user);
+    } catch (err) {
+      console.log(err);
+      return res.status(500).json(err);
+    }
+  },
+
+};
