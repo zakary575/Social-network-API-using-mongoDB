3ebe76860a0902a100577c5e13e9d0b7f2d050cf
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 12:17:36 -0600

    added the middleware to index.js for the routes

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const cwd = process.cwd();
 const PORT = process.env.PORT || 3001;
 const app = express();
 
-// Note: not necessary for the Express server to function. This just helps indicate what activity's server is running in the terminal.
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
