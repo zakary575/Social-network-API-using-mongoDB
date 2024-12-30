e0793cda1fa73a8ce270ed2bca60032597d504b5
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 11:02:08 -0600

    added the thought model

---
+++ models/Thought.js
@@ -0,0 +1,39 @@
+const { Schema, model } = require("mongoose");
+const reactionSchema = require("./Reaction");
+
+const thoughtSchema = new Schema(
+  {
+    thoughtId: {
+      type: Schema.Types.ObjectId,
+    },
+    thoughtText: {
+      type: String,
+      reuqire: true,
+      min_length: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+    },
+    username: {
+      type: String,
+      require: true,
+    },
+    reactions: [reactionSchema],
+  },
+  {
+    toJSON: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+thoughtSchema.virtual("reactionCount").get(function () {
+  return this.reactions.length;
+});
+
+const Thought = model("thought", thoughtSchema);
+
+module.exports = thoughtSchema;
