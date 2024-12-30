7f51b29b88d5bc8d77bf70395c71db5737415754
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 11:30:17 -0600

    added the function to delete a user

--- controllers/userController.js
+++ controllers/userController.js
@@ -4,7 +4,7 @@ module.exports = {
   // Get all users
   async getUsers(req, res) {
     try {
-      const users = await User.find().populate('students');
+      const users = await User.find().populate('thoughts');
       res.json(users);
     } catch (err) {
       res.status(500).json(err);
@@ -14,7 +14,7 @@ module.exports = {
   async getSingleUser(req, res) {
     try {
       const user = await User.findOne({ _id: req.params.userId })
-        .populate('students');
+        .populate('thoughts');
 
       if (!user) {
         return res.status(404).json({ message: 'No user with that ID' });
@@ -33,6 +33,21 @@ module.exports = {
     } catch (err) {
       console.log(err);
       return res.status(500).json(err);
+    }
+  },
+  // Delete a user
+  async deleteUser(req, res) {
+    try {
+      const user = await User.findOneAndDelete({ _id: req.params.userId });
+
+      if (!user) {
+        res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      await Student.deleteMany({ _id: { $in: user.thoughts } });
+      res.json({ message: 'User and thoughts deleted!' });
+    } catch (err) {
+      res.status(500).json(err);
     }
   },
 
