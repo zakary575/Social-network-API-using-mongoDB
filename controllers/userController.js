f1932780de1a0d21f7be188dab43013ff82c558e
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 11:28:00 -0600

    added the get single user function

--- controllers/userController.js
+++ controllers/userController.js
@@ -10,4 +10,19 @@ module.exports = {
       res.status(500).json(err);
     }
   },
+  // Get a user
+  async getSingleUser(req, res) {
+    try {
+      const user = await User.findOne({ _id: req.params.userId })
+        .populate('students');
+
+      if (!user) {
+        return res.status(404).json({ message: 'No user with that ID' });
+      }
+
+      res.json(user);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
 }
