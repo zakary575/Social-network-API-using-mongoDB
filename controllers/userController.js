fcf04bd093ee3d90ff1fc60dfde272568231aa99
Zakary Johnson <zakary.johnson575@gmail.com>
2024-12-30 11:25:36 -0600

    added the get all users function

---
+++ controllers/userController.js
@@ -0,0 +1,13 @@
+const {User} = require('../models')
+
+module.exports = {
+  // Get all users
+  async getUsers(req, res) {
+    try {
+      const users = await User.find().populate('students');
+      res.json(users);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  },
+}
